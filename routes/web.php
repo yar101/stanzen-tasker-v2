70d6f5848cc82dd2f4a5a763dd982724b9516c94
yar101 <yakimec@stnzn.ru>
2024-11-20 17:40:35 +0300

    Настроил руты для входа в приложение

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,16 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Dashboard', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-})->middleware('auth');
+    redirect('/dashboard');
+//    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+//    return Inertia::render('Dashboard', [
+//        'canLogin' => Route::has('login'),
+//        'canRegister' => Route::has('register'),
+//        'laravelVersion' => Application::VERSION,
+//        'phpVersion' => PHP_VERSION,
+//    ]);
+//})->middleware('auth');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
