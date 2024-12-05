b7eabc8f957b7d002982fa7ccffddb378cef20eb
yar101 <yakimec@stnzn.ru>
2024-12-05 10:23:03 +0300

    Сохранение позиции скролла после обновления задачи

--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -295,6 +295,7 @@ export default {
                     onSuccess: () => {
                         this.closeModals();
                     },
+                    preserveScroll: true,
                 },
             );
         },
